c7b103e33ab4372a7c9e380b36d4fff14e8d443e
AHKSS <98620270+AHKSS@users.noreply.github.com>
2025-04-14 16:55:13 +0300

    Create validate.js

---
+++ pages/api/validate.js
@@ -0,0 +1,7 @@
+import { getKey } from '@/lib/keyStore';
+
+export default function handler(req, res) {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const keyData = getKey(ip);
+  res.status(200).json(keyData);
+}
